6db0fe0c4739346fc595ec286084565c22e99d5b
Yash Raj <yashraj41323@gmail.com>
2026-09-08 23:06:26 +0530

    Fix formatting issues in main.py

--- main.py
+++ main.py
@@ -8,3 +8,4 @@ if __name__ == '__main__':
     # Retrieve port from environment, defaulting to 10000 for local/Render deployment
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
+
